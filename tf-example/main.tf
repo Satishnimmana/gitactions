9857ab9f1f3c99b75691f2853da70d80ef932fc8
Satishnimmana <satishnimmana31@gamil.com>
2022-10-04 16:40:01 +0530

    h

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -35,7 +35,7 @@ provider "aws" {
 
 resource "aws_instance" "myec2vm" {
   ami = data.aws_ami.amzlinux2.id
-  instance_type = " ${{ env.TF_VAR_INPUT_Name }}"
+  instance_type = "${{ env.TF_VAR_INPUT_Name }}"
   key_name      = "newkey"
 
   tags = {
