b2b4f541cdf8f94cd2dc3b8978a2571e110c07d0
Satishnimmana <satishnimmana31@gamil.com>
2022-10-18 17:56:02 +0530

    small changes MAP

--- tf-aws/variables.tf
+++ tf-aws/variables.tf
@@ -8,14 +8,22 @@ variable "ec2_instance_type" {
 variable "ec2_count"{
   type  = string
 }
-variable "region"{
-  type = string
-}
+#variable "region"{
+#  type = string
+#}
 variable "ami_name" {
   default = "ubuntu"
 
   validation {
     condition     = can(regex("amazon|RHEL|ubuntu", var.ami_name))
     error_message = "Invalid ami name, allowed_values = [amazon  RHEL ubuntu]."
+  }
+}
+
+variable "region" {
+  type = "map"
+  default = {
+    "Mumbai" = "ap-south-1"
+    "N.Viginia" = "us-east-1"
   }
 }
