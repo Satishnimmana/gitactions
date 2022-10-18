401741f17a2ae17b07d0576d1dfb5fb0b306a8b0
Satishnimmana <satishnimmana31@gamil.com>
2022-10-18 19:18:27 +0530

    volume

--- tf-ebs/main.tf
+++ tf-ebs/main.tf
@@ -3,8 +3,10 @@ provider "aws" {
   region  = var.region
 }
 resource "aws_ebs_volume" "ebs_volume" {
+#  volume_id   = aws_ebs_volume.ebs_volume.id 
   availability_zone = var.ebs_availability_zone
   size              = var.ebs_size
+  type              = "gp2"
   
   tags = {
     Name = var.ebs_name
