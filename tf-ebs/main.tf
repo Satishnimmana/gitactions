7a686c6fab2ebc4bafedbb869c391f8580ac8879
Satishnimmana <satishnimmana31@gamil.com>
2022-10-18 21:33:02 +0530

    hhg

--- tf-ebs/main.tf
+++ tf-ebs/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 resource "aws_ebs_volume" "ebsvolume" {
   availability_zone = "ap-south-1a"
-  volume size = var.volume_size
+  volume-size = var.volume_size
   encrypted = false
   tags = {
     name = "newvolume"
