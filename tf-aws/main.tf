fc979486d1a9928e618fd409f4c497ac02d3f6e0
Satishnimmana <98081327+Satishnimmana@users.noreply.github.com>
2022-12-09 13:38:47 +0530

    actions

--- tf-aws/main.tf
+++ tf-aws/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "linux" {
   ami           = data.aws_ami.os[var.ami_name].id
   instance_type = var.ec2_instance_type
-  key_name      = "windowskey"
+  key_name      = "actions"
   count = var.ec2_count
   tags = {
     Name= "${var.ec2_instance_name}.${var.environment}-ec2"
